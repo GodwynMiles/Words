04656e48f687608e895efb9d4f9508b07ed7b1e3
spago <spago@DESKTOP-CSM61C1>
2024-01-17 14:38:31 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+
+            bool found = false;
+
+            foreach (string word in words)
+            {
+                if (word.Contains(letter))
+                {
+                    Console.WriteLine(word);
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}
